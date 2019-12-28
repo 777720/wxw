83b646fef111a60cc7306eea58a2c882dd57ac5d
daFuShuai <627078086@qq.com>
2019-12-29 02:40:34 +0800

    es

--- xwx-vue/.eslintrc.js
+++ xwx-vue/.eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
     node: true
   },
   'extends': [
+    'plugin:vue/essential',
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
