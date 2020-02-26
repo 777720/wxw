95691e7d1a54ba66c76c357ac21c5831df7e8a9d
daFuShuai <627078086@qq.com>
2020-02-26 14:30:56 +0800

    2020-2-26

--- xwx-vue/src/core/product.js
+++ xwx-vue/src/core/product.js
@@ -439,6 +439,118 @@ const products = {
         },
         price: 320,
         upDate: '2019-5-22'
+      },
+      {
+        id: 'm-3-7',
+        chineseName: '白蓝衬衫',
+        name: 'BLUE&WHITE TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-7-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-7-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-7-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Blue/white|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-8',
+        chineseName: '白绿衬衫',
+        name: 'GREEN&WHITE TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-8-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-8-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-8-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Green/White|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-9',
+        chineseName: '白色冰淇淋衬衫',
+        name: 'MULTICOLOR TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-9-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-9-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-9-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Multicolor|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-10',
+        chineseName: '白紫衬衫',
+        name: 'PURPLE&WHITE TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-10-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-10-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-10-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Purple/white|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-11',
+        chineseName: '黑蓝拼接衬衫',
+        name: 'BLACK&BLUE TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-11-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-11-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-11-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Black/blue|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-12',
+        chineseName: '黑色亮片拼接衬衫',
+        name: 'BLUE&SILVER TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-12-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-12-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-12-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Blue/silver|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'm-3-13',
+        chineseName: '蓝色冰淇淋衬衫',
+        name: 'BLUE&SILVER TWEED/DENIM SHIRT',
+        url: [
+          { path: require('../assets/man/m-3-13-z.jpg'), type: 'z' },
+          { path: require('../assets/man/m-3-13-f.jpg'), type: 'f' },
+          { path: require('../assets/man/m-3-13-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': '70% Cotton|22%  Archylic|5%  Polyamide|2%  Polyester|1%  Wool|Multicolor/blue|Regular fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'XS/S/M/L'
+        },
+        price: 310,
+        upDate: '2020-2-26'
       }
     ],
     [
@@ -689,6 +801,7 @@ const products = {
         price: 490,
         upDate: '2019-7-10'
       },
+
       {
         id: 'w-1-1',
         chineseName: '音符开衫-黑色',
@@ -704,6 +817,70 @@ const products = {
         },
         price: 490,
         upDate: '2019-7-9'
+      },
+      {
+        id: 'w-1-2',
+        chineseName: '白蓝兔毛半拉链',
+        name: 'CHECK PRINT ZIPPED SHIRT',
+        url: [
+          { path: require('../assets/women/w-1-2-z.jpg'), type: 'z' },
+          { path: require('../assets/women/w-1-2-f.jpg'), type: 'f' },
+          { path: require('../assets/women/w-1-2-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': 'FABRIC|73%  POLYESTER|27%  RABBIT FUR|LINING|100%  POLYAMIDE|White/blue|Oversized fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'ONESIZE'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'w-1-3',
+        chineseName: '粉色兔毛半拉链',
+        name: 'CHECK PRINT ZIPPED SHIRT',
+        url: [
+          { path: require('../assets/women/w-1-3-z.jpg'), type: 'z' },
+          { path: require('../assets/women/w-1-3-f.jpg'), type: 'f' },
+          { path: require('../assets/women/w-1-3-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': 'FABRIC|73%  POLYESTER|27%  RABBIT FUR|LINING|100%  POLYAMIDE|White/pink|Oversized fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'ONESIZE'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'w-1-4',
+        chineseName: '灰色兔毛半拉链',
+        name: 'CHECK PRINT ZIPPED SHIRT',
+        url: [
+          { path: require('../assets/women/w-1-4-z.jpg'), type: 'z' },
+          { path: require('../assets/women/w-1-4-f.jpg'), type: 'f' },
+          { path: require('../assets/women/w-1-4-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': 'FABRIC|73%  POLYESTER|27%  RABBIT FUR|LINING|100%  POLYAMIDE|White/grey|Oversized fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'ONESIZE'
+        },
+        price: 310,
+        upDate: '2020-2-26'
+      },
+      {
+        id: 'w-1-5',
+        chineseName: '雾霾蓝兔毛半拉链',
+        name: 'CHECK PRINT ZIPPED SHIRT',
+        url: [
+          { path: require('../assets/women/w-1-5-z.jpg'), type: 'z' },
+          { path: require('../assets/women/w-1-5-f.jpg'), type: 'f' },
+          { path: require('../assets/women/w-1-5-t.jpg'), type: 't' }
+        ],
+        detail: {
+          'features': 'FABRIC|73%  POLYESTER|27%  RABBIT FUR|LINING|100%  POLYAMIDE|White/light blue|Oversized fit|Do not wash|Do not bleach|Do not tumble dry|Cool iron|Light dry cleaning',
+          'size': 'ONESIZE'
+        },
+        price: 310,
+        upDate: '2020-2-26'
       }
     ],
     [
